b5a816171b98ed5a400ba68c7b53b1ca287656af
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-05-31 07:43:32 +0530

    Add files via upload

---
+++ for_loop1.py
@@ -0,0 +1,4 @@
+# for loop in python
+Animals = ["Tiger","Lion","Elephant"]           # storage data from Animals variable
+for x in Animals:                               # suing for loop executing Animals line by line single data execution
+    print(x)                                    # print for single data by line by line for call
